bcdb5236730af82dbd0c67f31c615a8f13b713ff
Erfang Chen <erfangc@gmail.com>
2021-10-30 13:04:10 -0400

    work in progress

---
+++ prod/iam.tf
@@ -0,0 +1,54 @@
+resource "aws_iam_role" "ddb-stream-lambda-executor" {
+  name               = "DynamoDBStreamLambdaExecutioner"
+  assume_role_policy = jsonencode(
+  {
+    Version   = "2012-10-17",
+    Statement = [
+      {
+        Action    = "sts:AssumeRole",
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        },
+        Effect    = "Allow",
+        Sid       = "LambdaAssumeRole"
+      }
+    ]
+  }
+  )
+}
+
+resource "aws_iam_policy" "ddb-stream-lambda-executor-policy" {
+  name   = "DynamoDBStreamLambdaExecutionerPolicy"
+  policy = jsonencode({
+    Version   = "2012-10-17",
+    Statement = [
+      {
+        Action   = ["logs:*"],
+        Effect   = "Allow",
+        Resource = ["arn:aws:logs:*:*:*"]
+      },
+      {
+        Action   = [
+          "dynamodb:BatchGetItem",
+          "dynamodb:GetItem",
+          "dynamodb:GetRecords",
+          "dynamodb:Scan",
+          "dynamodb:Query",
+          "dynamodb:GetShardIterator",
+          "dynamodb:DescribeStream",
+          "dynamodb:ListStreams"
+        ],
+        Effect   = "Allow",
+        Resource = [
+          "*"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy_attachment" "ddb-stream-lambda-executor-policy-attachment" {
+  name       = "ddb-stream-lambda-executor-policy-attachment"
+  policy_arn = aws_iam_policy.ddb-stream-lambda-executor-policy.arn
+  roles      = [aws_iam_role.ddb-stream-lambda-executor.name]
+}
--- prod/lambda.tf
+++ prod/lambda.tf
@@ -1,35 +1,36 @@
-module "sync-assets-to-dev" {
-  source = "terraform-aws-modules/lambda/aws"
+data "archive_file" "replay-stream" {
+  output_path = "${path.module}/builds/replay-stream.zip"
+  source_dir  = "${path.module}/functions/replay-stream"
+  excludes    = ["__init__.py", "*.pyc"]
+  type        = "zip"
+}
 
-  function_name = "sync-ddb-table-assets-to-dev"
-  description   = "Reads off DynamoDB stream and writes the item to the dev environment"
-  handler       = "lambda_handler.lambda_handler"
-  runtime       = "python3.8"
+resource "aws_lambda_function" "sync-assets-to-dev" {
 
-  source_path = "./functions/replay-stream"
+  function_name    = "sync-ddb-table-assets-to-dev"
+  role             = aws_iam_role.ddb-stream-lambda-executor.arn
+  description      = "Reads off DynamoDB stream and writes the item to the dev environment"
+  handler          = "lambda_handler.lambda_handler"
+  filename         = data.archive_file.replay-stream.output_path
+  source_code_hash = data.archive_file.replay-stream.output_base64sha256
+  runtime          = "python3.8"
 
-  event_source_mapping = {
-    dynamodb = {
-      event_source_arn  = aws_dynamodb_table.assets.stream_arn
-      starting_position = "LATEST"
+  environment {
+    variables = {
+      TARGET_AWS_ACCOUNT_NUMBER = var.dev_account_id
+      TARGET_ROLE_NAME          = "arn:aws:iam::${var.dev_account_id}:role/ProductionDynamoDBSyncRole"
+      TARGET_DYNAMODB_NAME      = aws_dynamodb_table.assets.name
+      TARGET_REGION             = data.aws_region.current.name  
     }
-  }
-
-  allowed_triggers = {
-    dynamodb = {
-      principal  = "dynamodb.amazonaws.com"
-      source_arn = aws_dynamodb_table.assets.stream_arn
-    }
-  }
-
-  environment_variables = {
-    TARGET_AWS_ACCOUNT_NUMBER = var.dev_account_id
-    TARGET_ROLE_NAME          = "arn:aws:iam::${var.dev_account_id}:role/ProductionDynamoDBSyncRole"
-    TARGET_DYNAMODB_NAME      = aws_dynamodb_table.assets.name
-    TARGET_REGION             = data.aws_region.current.name
   }
 
   tags = {
     TableName = aws_dynamodb_table.assets.name
   }
+}
+
+resource "aws_lambda_event_source_mapping" "example" {
+  event_source_arn  = aws_dynamodb_table.assets.stream_arn
+  function_name     = aws_lambda_function.sync-assets-to-dev.arn
+  starting_position = "LATEST"
 }
