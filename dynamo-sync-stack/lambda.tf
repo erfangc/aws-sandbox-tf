1881beea78f9a31e6c3efe1d10b29fa10a7f3ef5
Erfang Chen <erfangc@gmail.com>
2021-10-30 21:03:14 -0400

    wip

--- dynamo-sync-stack/lambda.tf
+++ dynamo-sync-stack/lambda.tf
@@ -14,7 +14,7 @@ resource "aws_lambda_function" "lambda" {
   function_name    = "sync-ddb-table-${var.table_name}-to-dev"
   role             = aws_iam_role.ddb-stream-lambda-executor.arn
   description      = "Reads off DynamoDB stream and writes the item to the dev environment"
-  handler          = "lambda_handler.lambda_handler"
+  handler          = "main.lambda_handler"
   filename         = data.archive_file.code.output_path
   source_code_hash = data.archive_file.code.output_base64sha256
   runtime          = "python3.8"
