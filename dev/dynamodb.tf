0e66842a9038b6af5bb9255d28185ccf523bf2b2
Erfang Chen <erfangc@gmail.com>
2021-10-30 21:13:05 -0400

    added a table

--- dev/dynamodb.tf
+++ dev/dynamodb.tf
@@ -12,3 +12,18 @@ module "assets" {
   name                 = "assets"
   subnet_ids           = module.vpc.private_subnets
 }
+
+module "people" {
+  source               = "../dynamo-search-stack"
+  attributes           = [
+    {
+      name : "name",
+      type : "S"
+    }
+  ]
+  hash_key             = "name"
+  name                 = "people"
+  elasticsearch_domain = aws_elasticsearch_domain.esd.domain_name
+  security_group_id    = aws_security_group.lambda-sg.id
+  subnet_ids           = module.vpc.private_subnets
+}
